0a34291fe31fa7bb38e8e2b9d2f58994b4d30eb7
Vasil Shotarov <vshotarov@gmail.com>
2020-12-05 13:17:44 +0000

    A slighlty better day05 solution treating the codes as binary numbers

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -15,25 +15,13 @@ fn solve_part1(input: &str) -> std::io::Result<Vec<u32>> {
     let mut taken_seats: Vec<u32> = Vec::new();
 
     for line in input.lines() {
-        let mut row_left_pointer = 0;
-        let mut row_right_pointer = 127;
-        let mut col_left_pointer = 0;
-        let mut col_right_pointer = 7;
-
-        for letter in line.chars() {
-            match letter {
-                'F' => row_right_pointer -= (row_right_pointer - row_left_pointer) / 2 + 1,
-                'B' => row_left_pointer += (row_right_pointer - row_left_pointer) / 2 + 1,
-                'L' => col_right_pointer -= (col_right_pointer - col_left_pointer) / 2 + 1,
-                'R' => col_left_pointer += (col_right_pointer - col_left_pointer) / 2 + 1,
-                _ => println!("invalid char {}", letter),
-            }
-        }
-
-        let seat_id = row_left_pointer * 8 + col_left_pointer;
-
+        let seat_id: u32 = line.chars().rev().enumerate()
+            .fold(0, |acc, (i, letter)| match letter {
+                'F' | 'L' => acc,
+                'B' | 'R' => acc + (2 as u32).pow(i as u32),
+                _ => acc
+        });
         taken_seats.push(seat_id);
-
         if seat_id > highest_seat_id { highest_seat_id = seat_id; }
     }
 
