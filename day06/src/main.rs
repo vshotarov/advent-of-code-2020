88b8c28453b419b4b50f8735514911b4f02c0970
Vasil Shotarov <vshotarov@gmail.com>
2020-12-06 12:43:28 +0000

    Add Set based solutions to day06 Really surprising to me that the set based solution to part02 is slower than the naive string replacement one

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -12,21 +12,11 @@ fn main() -> std::io::Result<()> {
 }
 
 fn solve_part1(input: &str) -> std::io::Result<()> {
-    let mut yes_questions = 0;
-
-    for group in input.split("\n\n") {
-        let mut group_letters = group.replace("\n","");
-        loop {
-            let letter = match group_letters.chars().nth(0) {
-                Some(a) => { yes_questions += 1; a },
-                _ => break,
-            };
-
-            group_letters = group_letters.replace(letter, "");
-        }
-    }
-
-    println!("Part 1: {}", yes_questions);
+    println!("Part 1: {}", input
+        .split("\n\n")
+        .fold(
+            0, |acc, x| acc + HashSet::<char>::from(
+                x.replace("\n","").chars().collect()).len()));
 
     Ok(())
 }
@@ -51,6 +41,31 @@ fn solve_part2(input: &str) -> std::io::Result<()> {
         }
     }
 
+    println!("Part 2: {}", yes_questions);
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn solve_part2_with_sets(input: &str) -> std::io::Result<()> {
+    // Significantly slower than the naive part2 solution
+    // This one takes around 47ms on my laptop, while the other one
+    // takes around 18ms
+    let mut yes_questions = 0;
+    for group in input.split("\n\n") {
+        let mut sets_iter = group
+            .lines()
+            .map(
+                |x| HashSet::<char>::from(x.chars().collect()));
+
+        let mut first_set = sets_iter.nth(0).unwrap();
+
+        for _set in sets_iter {
+            first_set = &first_set & &_set; // intersection
+        }
+
+        yes_questions += first_set.len();
+    }
     println!("Part 2: {}", yes_questions);
 
     Ok(())
