39bd0353096ddb7d064e56724385beb4104da9a8
Vasil Shotarov <vshotarov@gmail.com>
2020-12-20 09:19:57 +0000

    Cleaned up the day16 solution as it was bugging me a lot

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -11,47 +11,25 @@ fn main() -> std::io::Result<()> {
 }
 
 fn solve_part1(input: &str) -> std::io::Result<()> {
-    let rules = input.split("\n\n").nth(0).unwrap();
-    let nearby_tickets = input.split("\n\n").nth(2).unwrap();
+    let parsed_rules: Vec<_> = input.split("\n\n").nth(0).unwrap().lines().map(
+        |line| move |x: u32| {
+            let rule1: Vec<u32> = line.split(": ").nth(1).unwrap()
+                .split(" or ").nth(0).unwrap().split("-")
+                .map(|num| num.parse::<u32>().unwrap()).collect();
+            let rule2: Vec<u32> = line.split(": ").nth(1).unwrap()
+                .split(" or ").nth(1).unwrap().split("-")
+                .map(|num| num.parse::<u32>().unwrap()).collect();
+            ((x >= rule1[0] && x <= rule1[1]) || (x >= rule2[0] && x <= rule2[1])) as bool
+        }).collect();
 
-    let parsed_rules = rules.lines().map(
-        |line| line.split(": ").nth(1).unwrap().split(" or ").map(
-            |constrain| {
-                let split_constrain: Vec<u32> = constrain.split("-").map(|x| x.parse::<u32>().unwrap()).collect();
-                (split_constrain[0], split_constrain[1])
-            }).collect::<Vec<(u32,u32)>>()).collect::<Vec<Vec<(u32,u32)>>>();
+    let nearby_tickets: Vec<Vec<u32>> = input.split("\n\n").nth(2).unwrap().lines()
+        .skip(1).map(|line| line.split(",").map(
+            |x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>()).collect();
 
-    let error_rate = nearby_tickets.lines().fold(0,
-        |acc, x| {
-            if x.contains("nearby") {
-                return 0;
-            }
-            acc + x.split(",").fold(0,
-                |_acc, _x| {
-                    let parsed_x = _x.parse::<u32>().unwrap();
-                    let mut valid = false;
-                    for rule in &parsed_rules {
-                        let mut local_valid = false;
-                        for constrain in rule {
-                            if parsed_x >= constrain.0 && parsed_x <= constrain.1 {
-                                local_valid = true;
-                                break;
-                            }
-                        }
-                        if local_valid {
-                            valid = true;
-                        }
-                        else {
-                        }
-                    }
-                    if !valid {
-                        _acc + parsed_x
-                    }
-                    else {
-                        _acc + 0
-                    }
-                })
-        });
+    let error_rate = nearby_tickets.iter().fold(0,
+        |acc, ticket| acc + ticket.iter().fold(0,
+            |ticket_acc, x| ticket_acc + x * (!parsed_rules.iter().fold(false,
+                |rule_acc, rule| rule_acc || rule(*x))) as u32));
 
     println!("Part 1: {}", error_rate);
 
@@ -60,127 +38,73 @@ fn solve_part1(input: &str) -> std::io::Result<()> {
 
 fn solve_part2(input: &str) -> std::io::Result<()> {
     let rules = input.split("\n\n").nth(0).unwrap();
-    let my_ticket = input.split("\n\n").nth(1).unwrap();
-    let nearby_tickets = input.split("\n\n").nth(2).unwrap();
+    let parsed_rules: Vec<_> = rules.lines().map(
+        |line| move |x: u32| {
+            let rule1: Vec<u32> = line.split(": ").nth(1).unwrap()
+                .split(" or ").nth(0).unwrap().split("-")
+                .map(|num| num.parse::<u32>().unwrap()).collect();
+            let rule2: Vec<u32> = line.split(": ").nth(1).unwrap()
+                .split(" or ").nth(1).unwrap().split("-")
+                .map(|num| num.parse::<u32>().unwrap()).collect();
+            ((x >= rule1[0] && x <= rule1[1]) || (x >= rule2[0] && x <= rule2[1])) as bool
+        }).collect();
 
-    let parsed_rules = rules.lines().map(
-        |line| line.split(": ").nth(1).unwrap().split(" or ").map(
-            |constrain| {
-                let split_constrain: Vec<u32> = constrain.split("-").map(|x| x.parse::<u32>().unwrap()).collect();
-                (split_constrain[0], split_constrain[1])
-            }).collect::<Vec<(u32,u32)>>()).collect::<Vec<Vec<(u32,u32)>>>();
+    let nearby_tickets: Vec<Vec<u32>> = input.split("\n\n").nth(2).unwrap().lines()
+        .skip(1).map(|line| line.split(",").map(
+            |x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>()).collect();
 
-    let valid_tickets = nearby_tickets.lines().filter(
-        |x| {
-            if x.contains("nearby") {
-                return false;
-            }
-            x.split(",").fold(0,
-                |_acc, _x| {
-                    let parsed_x = _x.parse::<u32>().unwrap();
-                    let mut valid = false;
-                    for rule in &parsed_rules {
-                        let mut local_valid = false;
-                        for constrain in rule {
-                            if parsed_x >= constrain.0 && parsed_x <= constrain.1 {
-                                local_valid = true;
-                                break;
-                            }
-                        }
-                        if local_valid {
-                            valid = true;
-                        }
-                        else {
-                        }
-                    }
-                    if !valid {
-                        _acc + parsed_x
-                    }
-                    else {
-                        _acc + 0
-                    }
-                }) == 0
-        }).collect::<Vec<&str>>();
-
-    let num_fields = parsed_rules.len();
-    let mut all_valid_positions: Vec<Vec<Vec<bool>>> = Vec::new();
-    for rule in &parsed_rules {
-        let mut rule_valid_positions: Vec<Vec<bool>> = Vec::new();
-        for ticket in &valid_tickets {
-            let mut valid_positions: Vec<bool> = vec![true; num_fields];
-            for (i, value) in ticket.split(",").enumerate() {
-                let parsed_value = value.parse::<u32>().unwrap();
-                if !((parsed_value >= rule[0].0 && parsed_value <= rule[0].1) ||
-                    (parsed_value >= rule[1].0 && parsed_value <= rule[1].1)) {
-                    valid_positions[i] = false;
-                }
-            }
-            rule_valid_positions.push(valid_positions);
-        }
-        all_valid_positions.push(rule_valid_positions);
-    }
+    let valid_tickets: Vec<&Vec<u32>> = nearby_tickets.iter().filter(
+        |ticket| ticket.iter().fold(true,
+            |ticket_acc, x| ticket_acc && parsed_rules.iter().fold(false,
+                |rule_acc, rule| rule_acc || rule(*x)))).collect();
 
     let num_rules = parsed_rules.len();
-    let mut rule_positions: Vec<i32> = vec![-1; num_rules];
-    let mut taken_positions: Vec<usize> = Vec::new();
 
-    loop {
-        for (i, rule_valid_positions) in all_valid_positions.iter().enumerate() {
-            if rule_positions[i] != -1 {
-                continue;
-            }
-            let mut possible_position = -1;
-            'outer: for j in 0..num_rules {
-                if taken_positions.contains(&j) {
+    let mut valid_positions: Vec<Vec<u32>> = Vec::new();
+    for rule in &parsed_rules {
+        let mut rule_possible_positions: Vec<bool> = vec![true; num_rules];
+        for ticket in &valid_tickets {
+            for position in 0..num_rules {
+                if !rule_possible_positions[position] {
                     continue;
                 }
-                for ticket in rule_valid_positions {
-                    if !ticket[j] {
-                        continue 'outer;
-                    }
-                }
-                if possible_position == -1 {
-                    possible_position = j as i8;
-                }
-                else {
-                    possible_position = -1;
-                    break;
-                }
-            }
-            if possible_position != -1 {
-                rule_positions[i] = possible_position as i32;
-                taken_positions.push(possible_position as usize);
+                rule_possible_positions[position] = rule_possible_positions[position] && rule(ticket[position]);
             }
         }
+        let mut rule_valid_positions: Vec<u32> = Vec::new();
+        for (i, position) in rule_possible_positions.iter().enumerate() {
+            if *position {
+                rule_valid_positions.push(i as u32);
+            }
+        }
+        valid_positions.push(rule_valid_positions);
+    }
 
-        if taken_positions.len() == num_rules - 1{
-            let mut to_assign = 0;
-            let mut assign_to = 0;
-            for (j, rule) in rule_positions.iter().enumerate() {
-                if *rule == -1 {
-                    assign_to = j;
-                    for position in 0..num_rules {
-                        if !taken_positions.contains(&position) {
-                            to_assign = position;
-                            break;
-                        }
-                    }
+    let mut rule_positions: Vec<u32> = vec![1000; num_rules];
+    loop {
+        for i in 0..num_rules {
+            if valid_positions[i].len() != 1 {
+                continue;
+            }
+            rule_positions[i] = valid_positions[i].pop().unwrap();
+            for j in 0..num_rules {
+                match valid_positions[j].iter().position(|x| *x == rule_positions[i]) {
+                    Some(x) => { valid_positions[j].remove(x); () },
+                    None => ()
                 }
             }
-            rule_positions[assign_to] = to_assign as i32;
+        }
+        if !rule_positions.contains(&1000) {
             break;
         }
     }
+    let my_ticket: Vec<u64> = input.split("\n\n").nth(1).unwrap().lines().nth(1).unwrap()
+        .split(",").map(|x| x.parse::<u64>().unwrap()).collect();
+    let departure_fields: Vec<usize> = rules.lines().enumerate()
+        .filter(|(i,x)| x.contains("departure")).map(|(i,x)| i).collect();
 
-    let mut departure_fields_product = 1;
-    for (i, rule) in rules.lines().enumerate() {
-        if rule.contains("departure") {
-            departure_fields_product *= my_ticket.lines().nth(1).unwrap().split(",").nth(
-                rule_positions[i] as usize).unwrap().parse::<u64>().unwrap();
-        }
-    }
-
-    println!("Part 2: {}", departure_fields_product);
+    println!("Part 2: {}", departure_fields.iter().fold(1,
+            |acc,x| acc * my_ticket[rule_positions[*x] as usize]));
 
     Ok(())
 }
